ffce60dcdb92e9d7f9f36e0688bbd99d450cd3bf
Masazumi HAYASHI <hayashimasazumi@gmail.com>
2013-09-03 05:47:36 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+My0903::Application.config.secret_key_base = 'a3aee655017663b2861be2534e1bc1a480ff41100d73932e64f75603a8ea522aef61cd403e3fac10354086e32f7517db78ad16d7acd225a4b8353339d8936e03'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+My0903::Application.config.session_store :cookie_store, key: '_my0903_session'
